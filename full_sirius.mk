e37ed59abbf594edfc4942c822a2253094bc5e97
nailyk-fr <nailyk_git@nailyk.fr>
2017-04-14 00:28:52 +0200

    sirius: twrp: Add twrp dependencies
    Change-Id: I9e017f4b5b4f0d95d07da2a7dc25548484758cbd

--- full_sirius.mk
+++ full_sirius.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from sirius device
 $(call inherit-product, device/sony/sirius/sirius.mk)
+$(call inherit-product, device/sony/sirius/twrp.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_sirius
